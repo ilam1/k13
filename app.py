2f0e0cc60ee48040c13fbdee5c05464b91f91fd9
Irene Lam <ilam1@stuy.edu>
2017-11-12 00:40:48 -0500

    WOT template

--- app.py
+++ app.py
@@ -10,12 +10,21 @@ app = Flask (__name__)
 
 @app.route("/")
 def hello_world():
-    data = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=PFlG6OFpBTAvtvWaeKFaRyh9keK3FRezj9cQyug2")
-    url = data.geturl()
-    head = data.info()
-    content = data.read()
-    d = json.loads(content)
-    return render_template("nasa.html", pic = d["url"], content = d["explanation"])
+    try:
+        data = urllib2.urlopen("http://api.mywot.com/0.4/public_link_json2?hosts=google.com/&callback=process&key=d0cc56daa6619b889f626ef9f0766eff582df53b")
+        datam = data.read()[8:-1]
+        d = json.loads(datam)
+        print("json ->", d)
+        return render_template("wot.html", level=d[d.keys()[0]]['categories'], content=d[d.keys()[0]]['target'])
+    except urllib2.URLError as e:
+        print e.message
+        print "something went wrong"
+    # url = data.geturl()
+    # print url
+    # head = data.info()
+    # print head
+    return render_template("nasa.html")
+
 
 if __name__ == "__main__":
     app.debug = True
